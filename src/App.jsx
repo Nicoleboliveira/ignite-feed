06e6ff1141166d58ed2d32e3b2f6ad1fb65d3aec
nicole <nicoleboliveira14@gmail.com>
2025-03-31 18:23:14 -0300

    Iniciando o sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
     <Header />
-     <Post 
-      author= "Nicole Brenda" 
-      content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit blanditiis, laborum quia veniam commodi vel hic ipsa dolorem, atque obcaecati amet aliquid ut, odio deserunt voluptatem corrupti! Laboriosam, quod. Culpa."
-     />
-     <Post 
-      author= "Heitor Leite " 
-      content="Um novo post muito legal."
-     />
+     
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post
+         author="Nicole Brenda"
+         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facilis beatae placeat, exercitationem ipsum voluptates saepe at necessitatibus sequi quas, autem, ad incidunt reiciendis magnam consequatur provident perferendis aut! Quaerat!"
+        />
+        <Post
+         author="Bianca Amorim"
+         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima quos architecto excepturi molestias, illo voluptate obcaecati laudantium? Consequatur autem eum officiis perspiciatis tempora inventore at a quas doloremque ea! Odit?"
+        />
+      </main>
+     </div>
     
     </div>
   )
 }
+
 
 
