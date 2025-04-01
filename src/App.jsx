a25fc48fea0ef617bdcb266189e64c81774d036b
nicole <nicoleboliveira14@gmail.com>
2025-04-01 19:19:49 -0300

    Formatando o codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Header } from './components/Header'
-import { Post } from './components/Post'
-import { Sidebar } from './components/Sidebar'
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
-import './global.css'
-import styles from './App.module.css'
+import "./global.css";
+import styles from "./App.module.css";
 
 //author: {avatar_url:"", name:"", role:""}
 //publishedAt: Date
@@ -13,61 +13,65 @@ const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/Nicoleboliveira.png',
-      name: 'Nicole Brenda',
-      role: 'Frontend'
+      avatarUrl: "https://github.com/Nicoleboliveira.png",
+      name: "Nicole Brenda",
+      role: "Frontend",
     },
     content: [
-      { type:'paragraph', content: 'Fala galeraa 👋'},
+      { type: "paragraph", content: "Fala galeraa 👋" },
 
-      { type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
 
-      { type:'link', content:'👉 jane.design/doctorcare'},
+      { type: "link", content: "👉 jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2025-04-01 09:59:00')
+    publishedAt: new Date("2025-04-01 09:59:00"),
   },
 
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/heitorleite18.png',
-      name: 'Heitor Leite',
-      role: 'Full-Stack'
+      avatarUrl: "https://github.com/heitorleite18.png",
+      name: "Heitor Leite",
+      role: "Full-Stack",
     },
     content: [
-      { type:'paragraph', content: 'Fala galeraa 👋'},
+      { type: "paragraph", content: "Fala galeraa 👋" },
 
-      { type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
 
-      { type:'link', content:'👉 jane.design/doctorcare'},
+      { type: "link", content: "👉 jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2025-03-31 08:30:00')
+    publishedAt: new Date("2025-03-31 08:30:00"),
   },
-]
+];
 
 export function App() {
   return (
     <div>
-    <Header />
-     
-     <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-        {posts.map(post => {
-          return ( 
-          <Post 
-            author={post.author}
-            content={post.content}
-            publishedAt={post.publishedAt}
-          />
-        )
-        })}
-      </main>
-     </div>
-    
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,35 +1,38 @@
-import { ThumbsUp, Trash } from '@phosphor-icons/react'
-import styles from './Comment.module.css'
-import { Avatar } from './Avatar'
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
-    return (
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/Nicoleboliveira.png" />
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/Nicoleboliveira.png" />
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Nicole Brenda</strong>
-                            <time title='01 de Abril às 09:59' dateTime="2025-04-01 09:59"> Cerca de 1h atrás</time>
-                        </div>
-                        
-                        <button title="Deletar comentário">
-                            <Trash size={24}/>
-                        </button>
-                    </header>
-
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Nicole Brenda</strong>
+              <time title="01 de Abril às 09:59" dateTime="2025-04-01 09:59">
+                {" "}
+                Cerca de 1h atrás
+              </time>
             </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
