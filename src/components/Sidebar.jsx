1539849740eec1d752779f93e8a53b42b68a3ea0
nicole <nicoleboliveira14@gmail.com>
2025-04-01 14:12:53 -0300

    Componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilSimpleLine } from '@phosphor-icons/react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
@@ -9,7 +10,7 @@ export function Sidebar() {
            src="https://images.unsplash.com/photo-1551970634-747846a548cb?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Nicoleboliveira.png" />
+                <Avatar  src="https://github.com/Nicoleboliveira.png"/>
                 <strong> Nicole Brenda</strong>
                 <span>Frontend</span>
             </div>
