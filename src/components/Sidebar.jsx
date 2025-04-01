e5e18f7e3a612ac5cb234a69889b36ee3755e870
nicole <nicoleboliveira14@gmail.com>
2025-04-01 09:42:04 -0300

    Finalizado sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css'
 export function Sidebar() {
     return (
@@ -7,12 +9,14 @@ export function Sidebar() {
            src="https://images.unsplash.com/photo-1551970634-747846a548cb?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Nicoleboliveira.png" />
                 <strong> Nicole Brenda</strong>
                 <span>Frontend</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilSimpleLine size={20} weight="bold"/>
                   Editar seu perfil
                 </a>
             </footer>
